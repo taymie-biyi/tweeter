a9018d4200e37b1a51000fd52c0d61f462626fee
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-26 05:38:38 -0500

    Update counter to change color ro red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,8 +5,13 @@ $(document).ready(function() {
     let count = $(this).val().length;
     const charCount = maxLength - count;
     const counter = $(this).parent()[0][2];
-
     $(counter).text(charCount)
+
+    if (charCount < 0) {
+      $(counter).addClass('red-counter')
+    } else {
+      $(counter).removeClass('red-counter');
+    }
   });
 
 });

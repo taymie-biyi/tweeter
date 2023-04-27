8ccc4af2e2a1391de279dc25fb92b259f4a1ee98
Temiloluwa Olubiyi <temiloluwa.olubiyi@yahoo.com>
2023-04-27 07:57:59 -0500

    Fetch tweets with AJAX, and view timestamp using timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 const createTweetElement = function($tweetObj) {
   const $tweet = $(`
   <article class="tweet">
@@ -13,7 +14,7 @@ const createTweetElement = function($tweetObj) {
     </header>
     <p>${$tweetObj.content.text}</p>
     <footer>
-      <small>${$tweetObj.created_at}</small>
+      <small>(${timeago.format($tweetObj.created_at)})</small>
       <div>
         <small>
           <i class="fas fa-solid fa-flag"></i>
@@ -26,30 +27,6 @@ const createTweetElement = function($tweetObj) {
   return $tweet;
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -60,10 +37,22 @@ const renderTweets = function(tweets) {
     $('#tweets-container').prepend(newTweet);
   }
 }
+
 $(document).ready(function() {
   
-  renderTweets(data);
+  // renderTweets(data);
 
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    }).then((tweets) => {
+      renderTweets(tweets);
+    })
+  }
+
+  loadTweets();
+  
   //grab form from DOM
   const $form = $('#create-tweet');
 
@@ -71,6 +60,12 @@ $(document).ready(function() {
     event.preventDefault();
     const data = $form.serialize();
     console.log(data)
+
+    $.ajax({
+      method: POST,
+      url: '/tweets/',
+      data: data
+    })
   })
 });
 
